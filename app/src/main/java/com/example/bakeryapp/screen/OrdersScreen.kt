bdc965376a74d1b1d7c56e5127d45ad19e345cdf
ziv-madar <58150490+ziv-madar@users.noreply.github.com>
2022-12-07 18:03:43 +0200

    OrderScreen styles

--- app/src/main/java/com/example/bakeryapp/screen/OrdersScreen.kt
+++ app/src/main/java/com/example/bakeryapp/screen/OrdersScreen.kt
@@ -1,5 +1,7 @@
 package com.example.bakeryapp.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -8,32 +10,30 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+
 import androidx.navigation.NavController
 import com.example.bakeryapp.util.OrdersData
 import com.example.bakeryapp.util.SharedViewModel
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firestore.v1.StructuredQuery.Order
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+
 
 @Composable
 fun OrdersScreen(
     navController: NavController,
-    sharedViewModel: SharedViewModel
+    sharedViewModel: SharedViewModel,
 ) {
-    val context = LocalContext.current
+
     val orders: MutableState<List<OrdersData>> = remember {
         mutableStateOf(listOf())
     }
 
+    /* runs only if 'orders' change'*/
     LaunchedEffect(key1 = orders) {
-       val ordersData = sharedViewModel.getOrders().await()
+        val ordersData = sharedViewModel.getOrders().await()
         orders.value = ordersData
     }
 
@@ -44,27 +44,32 @@ fun OrdersScreen(
                 .padding(start = 15.dp, top = 15.dp)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.Start
-        ){
+        ) {
             IconButton(
                 onClick = {
                     navController.popBackStack()
                 }
-            ){
+            ) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back button")
             }
         }
+
         /* @TODO Fetch items for each order list + Styling */
-        for(order in orders.value) {
-            Text(text = "The order is from user: ${order.userId}")
-            Row {
-            for(item in order.list) {
-                Column {
-                    Text(text = "Item id:${item.itemId}")
-                    Text(text = "Item Amount: ${item.amount}")
+        for (order in orders.value) {
+            Column(modifier = Modifier
+                .background(Color.LightGray)
+                .padding(16.dp)) {
+                Text(text = "The order is from user: ${order.userId}")
+                Row {
+                    for (item in order.list) {
+                        Column {
+                            Text(text = "Item id:${item.itemId}")
+                            Text(text = "Item Amount: ${item.amount}")
+                        }
+                    }
                 }
             }
-            }
         }
-
     }
+
 }
